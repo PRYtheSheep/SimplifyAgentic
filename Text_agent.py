c859f66e21c405344201477c326f6251c4a9557d
unknown <dominiclaimx@gmail.com>
2025-09-02 19:21:50 +0800

    Updated text_agent.py

--- Text_agent.py
+++ Text_agent.py
@@ -1,6 +1,9 @@
 import json
 import logging
 import os
+from datetime import datetime
+import re
+
 import boto3 
 from botocore.exceptions import ClientError
 
@@ -29,10 +32,12 @@ TAVILY_API_KEY = os.getenv('TAVILY_API_KEY', None)
 bedrock_model = BedrockModel(
     # model_id="us.amazon.nova-lite-v1:0",
     # model_id="anthropic.claude-3-5-sonnet-20240620-v1:0",
-    # model_id = "anthropic.claude-3-haiku-20240307-v1:0",
-    model_id="anthropic.claude-3-5-sonnet-20240620-v1:0",
+    model_id = "anthropic.claude-3-haiku-20240307-v1:0",
+    # model_id="anthropic.claude-3-5-sonnet-20240620-v1:0",
     temperature=0.1,
-    region_name="ap-southeast-1"
+    region_name="ap-southeast-1",
+    top_p=0.9,
+    # max_tokens = 300
 )
 
 if TAVILY_API_KEY:
@@ -48,7 +53,7 @@ else:
 
 
 @tool
-def web_search(query: str, max_results: int = 5):
+def web_search(query: str, max_results: int = 3):
     """
     Perform an internet search with the specified query
     
@@ -64,118 +69,137 @@ def web_search(query: str, max_results: int = 5):
     print("*"*20)
     print(f"query: {query}")
     print(f"max_results: {max_results}")
+    print(f"search start: {datetime.now()}")
     print("*"*20)
     domains = ["straitstimes.com/singapore","channelnewsasia.com/singapore","mothership.sg","todayonline.com"]
     # exclude_tags = ["listen","tag","topic"]
     # domains_excluded = [i+"/"+j for i in domains for j in exclude_tags]
     # print(domains_excluded)
     if TAVILY_API_KEY:
-        response = tavily_client.search(
-            query,
-            max_results = max_results,
-            time_range = "month",
-            include_domains=domains,
-            exclude_domains = ["youtube.com","instagram.com"],
-            include_answer = "advanced",
-            search_depth = "advanced"
-            # include_raw_content = True
-            # include_images = True
-            # start_date = "2025-01-01",
-            # end_date = "2025-02-01" 
-        )
+        try:
+            response = tavily_client.search(
+                query,
+                max_results = max_results,
+                time_range = "month",
+                include_domains=domains,
+                exclude_domains = ["youtube.com","instagram.com"],
+                include_answer = "advanced",
+                search_depth = "advanced"
+                # include_raw_content = True
+                # include_images = True
+                # start_date = "2025-08-01",
+                # end_date = "2025-08-30" 
+            )
+        except Exception as e:
+            print(f"\nAn error occurred: {str(e)}")
     else:
         response = DDGS().text(
             "python programming",
             max_results = max_results
         )
     
-    summary = response["answer"]
     urls=[]
     titles = []
     contents = []
-    scores = []
+    domains = []
     for article in response["results"]:
         if article["score"]>0.6:
+            for domain in domains:
+                pattern = re.escape(domain)
+                if re.search(pattern, article["url"]):
+                    domains.append(domain)
             urls.append(article["url"])
             titles.append(article["title"])
             contents.append(article["content"])
-            scores.append(article["score"])
+    articledict = {}
+    articledict["summary"] = response["answer"]
+    articledict["domains"] = domains
+    articledict["urls"] = urls
+    articledict["titles"] = titles
+    articledict["contents"] = contents
     # print(type(response))
     # print(len(response))
     # print(response)
-    # print(json.dumps(response, indent=2, default=str))
+    print(json.dumps(response, indent=2, default=str))
     # print(summary, titles, urls)
-    return summary, titles, urls
+    print(f"search done: {datetime.now()}")
+    # domain title content [str] , [str] , [str]
+    return articledict
 
-# class TextAgentModel:
-#     def __init__(self, model_id: str, region_name: str = "ap-southeast-1"):
-#         self.model_id = model_id
-#         self.bedrock_client = boto3.client('bedrock-runtime', region_name=region_name)
-#     def generate_search_query(self,evidence):
-#         request_body = 
-#         try:
-#             response = self.bedrock_client.invoke_model(
-#                     modelId=self.model_id,
-#                     body=json.dumps(request_body),
-#                     contentType='application/json'
-#                 )
-#         except Exception as e:
-#             return f"Error: {str(e)}"
 def analyse_text( prompt):
-    # SYSTEM_PROMPT = """
-    #     You are a text analyst agent whose goal is to receive provided details.
-        
-    #     1. Determine if the input is real or falsified
-    #     2. If you wish to perform a web search, generate different prompts
-    #     3. Use your research tools (web_search, http_request,) to find relevant information
-    #     4. Include source URLs and keep findings under 500 words
-    # """
-    SYSTEM_PROMPT = """
-    Firstly, perform an analysis of the input content.
-    1. Extract all factual claims, statistics, dates, names, events, and assertions from the chat
-    2. Categorize claims by type (historical facts, current events, statistics, quotes, etc.)
-    3. Identify claims that are most critical to verify
 
-    Secondly, for each unsubstantiated claim, generate tp to 3 search queries with the following guidelines:
-    1. Use specific, fact-focused keywords
-    2. Include dates, names, and context when relevant
-    3. Vary phrasing and sentence structure for different queries
-
-    Next, use the web-search tool to obtain evidence from reputable sources.
-    Finally, for each claim, assign one of these categories with reasons for the classification:
-
-    VERIFIED: Confirmed by reliable sources
-    FALSE: Contradicted by reliable evidence
-    MISLEADING: Partially true but lacking context or nuance
-    UNVERIFIABLE: Insufficient evidence available
-    OPINION: Subjective statement, not factual claim
-
+    """
+    Extracts factual claims from evidence.
+    Generates web search queries. 
+    Calls web_search function to run queries
     
-    For each significant claim, return output in the following format:
-    Claim: [Quote the exact statement]
-    Status: [VERIFIED/FALSE/MISLEADING/UNVERIFIABLE/OPINION]
-    Evidence: [Brief explanation with source citations]
-    Source URLs: [List relevant URLs]
+    Args:
+        query (str): A question or search phrase to perform a search with
+        max_results (int): Number of results to return for each web search
+        
+    Returns:
+        verified_evidence (List(dict)) : List containing a dictionary for each claim extracted
+    """
 
-"""
-    text_agent = Agent(
+    SYSTEM_PROMPT1 = """
+    Extract factual claims. 
+    List each in <claim1></claim1>, <claim2></claim2> format.
+    Suggest possible web search queries for each claim.
+    List them in <query1></query1>, <query2></query2> format. 
+    
+    """
+
+    extraction_agent = Agent(
         model = bedrock_model,
-        system_prompt = SYSTEM_PROMPT,
-        tools = [web_search, http_request],
+        system_prompt = SYSTEM_PROMPT1,
+        tools = [],
         callback_handler = None
     )
-    response = text_agent(
-        f"Analyse this evidence {prompt}"
+    extracted_response = extraction_agent(
+        f"Analyse this {prompt}"
         )
-    print(str(response))
+    # print(type(extracted_response))
+    print(str(extracted_response))
+
+    pattern = re.compile(r"<claim(\d+)>(.*?)</claim\1>\s*<query\1>(.*?)</query\1>", re.DOTALL)
+
+    claims = []
+    queries = []
+    for match in pattern.findall(str(extracted_response)):
+        idx, claim, query = match
+        claims.append(claim.strip())
+        queries.append(query.strip())
+    # print(claims)
+    # print(queries)
+    verified_evidence = []
+    for i in range(len(queries)):
+        web_result = web_search(queries[i])
+        web_result["claim"] = claims[i]
+        verified_evidence.append(web_result)
+    return verified_evidence
+
+
+
+    # response = text_agent(
+    #     f"These are the claims: {str(extracted_response)}"
+    #     )
+    # print(str(response))
         
 
 if __name__ == "__main__":
-    # summary, titles, urls = web_search("SengKang Green Primary School bullying")
-    # print(f"summary: {summary}")
-    # print(f"titles: {titles}")
-    # print(f"urls: {urls}")
-    analyse_text(
-    """A primary 6 student was bullied in Sengkang Green primary school. 
-    Perform a maximum of 2 web searches""")
+
+    start_time = datetime.now()
+    print(f"Start Time: {start_time}")
+    strlist = ["A primary 3 student was bullied in Sengkang Green primary school. ",
+               "A chinese student was bullied in Sengkang Green primary school.",
+               "Singaporeans received CDC vouchers in 2025."]
+    # for i in strlist:
+    #     analyse_text(
+    #     i)
+    # analyse_text(strlist[0])
+    print(analyse_text(strlist[0] + strlist[1] + strlist[2]))
+    # A chinese student was bullied in Sengkang Green primary school.
+    # Singaporeans received CDC vouchers in 2025.
+    # print(web_search("A secondary 2 student was bullied in Sengkang Green primary school."))
+    print("Elapsed time:", (datetime.now()-start_time).total_seconds())
 
